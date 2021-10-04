93a21aa813436688ab5c28e628663bd89e7f1e11
ria10 <ria1099@gmail.com>
2021-10-04 16:30:13 +0100

    adding seeds

---
+++ db/2_seed_tables.sql
@@ -0,0 +1,18 @@
+INSERT INTO habits (habitName, frequency, startDate, targetDate, habitType, userId)
+VALUES
+(
+    'water',
+    8,
+    2021-10-04,
+    2021-10-05,
+    true,
+    1
+),
+(
+    'sleep',
+    12,
+    2021-10-04,
+    2021-10-05,
+    true,
+    1
+);
