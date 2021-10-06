5b962c7b484048f87c91f824f29326d16e18d420
ria10 <ria1099@gmail.com>
2021-10-06 10:46:00 +0100

    changing path for .env file

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,12 +1,8 @@
-require("dotenv").config();
-
-SECRET =
-  "f560bf0c02e7fa51af64064111a8ab8c40cebc967b849e60f7bdb6bcc25aa82d2e50846f15cfd8e1057a3cb7658962c12cf8967bb7f62b2671f24836ceaac6df";
+require("dotenv").config({path: __dirname + '/./../.env'});
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-<<<<<<< HEAD
 const User = require("../models/users");
 
 async function create(req, res) {
@@ -28,7 +24,8 @@ async function checkLogin(req, res) {
     }
     const authed = bcrypt.compare(req.body.password, user.passwordHash);
     if (!!authed) {
-      const payload = { username: user.userName };
+      console.log(user.userId);
+      const payload = { username: user.userName, id: user.userId };
       const sendToken = (err, token) => {
         if (err) {
           throw new Error("Error in token generation");
@@ -38,24 +35,9 @@ async function checkLogin(req, res) {
           token: "Bearer " + token,
         });
       };
-      jwt.sign(payload, SECRET, sendToken);
+      jwt.sign(payload, process.env.SECRET, sendToken);
     } else {
       throw new Error("User could not be authenticated");
-=======
-async function create(req, res)  {
-    try {
-        console.log("hello")
-        const salt = await bcrypt.genSalt();
-        const hashed = await bcrypt.hash(req.body.password, salt);
-        console.log("hello" + hashed);
-        //if (!User.findById(req.body)) {
-        await User.create({...req.body, passwordHash: hashed});
-        res.status(201).json({message: "User has been created successfully"});
-    //}
-    //else(err)
-    } catch (err) {
-        res.status(500).json({err});
->>>>>>> a43926d639ae5ee4c23ea1f4e5b764944855a5d8
     }
   } catch (err) {
     console.log(err);
